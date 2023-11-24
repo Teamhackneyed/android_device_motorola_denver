b40dd3f7415eca875efcf28b2a483c4cdf43e920
Vivekachooz <vivekachooz@gmail.com>
2023-11-24 19:08:51 +0530

    denver: Import and adapt powerhint from redfin
    * From redfin-user-13-TQ3A.230901.001-10750268-release-keys
    
    Change-Id: I923c1adf70d035c9def0032ccf29530ce92ebe66

--- device.mk
+++ device.mk
@@ -62,6 +62,9 @@ $(call soong_config_set,livedisplay_sysfs,enable_ab,true)
 # Power
 $(call soong_config_set,qtipower,tap_to_wake_node,/sys/class/touchscreen/primary/gesture)
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
