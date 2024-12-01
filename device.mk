e0c469679a4699ac18d3dd23b0b068d6481ebf7d
Michael Bestas <mkbestas@lineageos.org>
2024-12-02 01:29:18 +0200

    fogos: Convert rootdir to init
    Change-Id: I43ccc55b98231d984474e6bba3c9fa1266bb7351

--- device.mk
+++ device.mk
@@ -40,12 +40,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Init
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/hw/*.rc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/*.rc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
+PRODUCT_PACKAGES += \
+    init.mmi.overlay.rc \
+    init.oem.fingerprint2.sh
 
 # NFC
 PRODUCT_PACKAGES += \
