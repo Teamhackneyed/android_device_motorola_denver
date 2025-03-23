d80616e6fd1bf1df9fb3ec8f3f4b57771c0c9d39
Michael Bestas <mkbestas@gmail.com>
2025-03-23 09:08:13 +0200

    fogo: Update proprietary files from stock
    Change-Id: Ib43888eb4f73c6c597c0d1b201e45985806ad1be

--- extract-files.py
+++ extract-files.py
@@ -30,11 +30,6 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'vendor/etc/init/android.hardware.nfc@1.2-service.sec.rc': blob_fixup()
-        .regex_replace('sec', 'samsung')
-        .regex_replace('class hal\n    user nfc', 'override\n    class hal\n    user nfc'),
-    'vendor/lib64/libmot_chi_desktop_helper.so': blob_fixup()
-        .add_needed('libgui_shim_vendor.so'),
     'vendor/lib64/sensors.moto.so': blob_fixup()
         .add_needed('libbase_shim.so'),
 }  # fmt: skip
